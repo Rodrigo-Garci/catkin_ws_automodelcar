1a6b23c47818efe9e1dd9a03bf48107f77ea3b8c
Rodrigo-Garci <al1609049@upy.edu.mx>
2020-03-06 11:38:45 -0600

    inicial

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/rodrigo/catkin_ws_automodelcar/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/rodrigo/catkin_ws_automodelcar/devel;/home/rodrigo/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/rodrigo/catkin_ws_automodelcar/devel;/home/rodrigo/catkin_ws/devel;/opt/ros/melodic" != "" else []
